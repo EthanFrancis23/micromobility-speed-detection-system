a090f720ab51598778a488d8cc41961b2abf0a9b
stimmins23 <stimmins234@gmail.com>
2026-03-24 10:23:35 -0400

    - uninstalled and reinstalled venv and dependencies - created CameraCapture class to capture images and save images with timestamps in src/data/captures - updated main to test Camera and save images

--- src/camera/capture.py
+++ src/camera/capture.py
@@ -4,3 +4,31 @@ Camera capture module.
 Takes a photo when the configured speed threshold is exceeded and saves it with a timestamp.
 
 """
+
+from pathlib import Path
+from datetime import datetime
+import time
+
+from picamera2 import Picamera2
+
+from src.config.settings import CAPTURE_DIR
+
+class CameraCapture:
+    def __init__(self) -> None:
+        self.capture_dir = Path(CAPTURE_DIR)
+        self.capture_dir.mkdir(parents=True, exist_ok=True)
+        self.camera = Picamera2()
+
+
+    def capture_image(self, prefix: str ="capture") -> Path:
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%s")
+        file_path = self.capture_dir / f"{prefix}_{timestamp}.jpg"
+
+        config = self.camera.create_still_configuration()
+        self.camera.configure(config)
+        self.camera.start()
+        time.sleep(2)
+        self.camera.capture_file(str(file_path))
+        self.camera.stop()
+
+        return file_path
--- src/main.py
+++ src/main.py
@@ -10,11 +10,22 @@ Starts the radar, LCD, and camera services. Then runs the main monitoring loop.
 import time
 
 from src.display.lcd_display import LCDDisplay
+from src.camera.capture import CameraCapture
 
 def main() -> None:
     lcd = LCDDisplay()
     lcd.show_startup()
     time.sleep(2)
+
+
+    lcd.write_lines("Testing Camera", "Say Cheese!")
+
+    camera = CameraCapture()
+    image_path = camera.capture_image(prefix="test")
+
+    lcd.write_lines("Image Saved", image_path.name[:16])
+    time.sleep(3)
+
     lcd.show_idle()
 
 
