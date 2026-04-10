94a60cbc8be3a8a995503039b26df44cd4034de3
stimmins23 <stimmins234@gmail.com>
2026-04-10 15:36:17 -0400

    - updated api/routs/events.py to include schema event model to plug in the route

--- src/api/routes/events.py
+++ src/api/routes/events.py
@@ -6,8 +6,20 @@ Event endpoints
 
 from fastapi import APIRouter
 from src.api.schemas.event import Event
+from src.database import get_all_events
+from src.models.speed_event_model import SpeedEvent
 
 router = APIRouter()
+
+def to_event_schema(event: SpeedEvent) -> Event:
+    return Event(
+        id=event.id,
+        timestamp=event.timestamp.isoformat(),
+        speed_mph=event.speed_mph,
+        threshold_value=event.threshold.value,
+        image_path=event.image_path,
+        location=event.location,
+    )
 
 sample_events = [
     Event(
@@ -38,4 +50,5 @@ sample_events = [
 
 @router.get("/events", response_model=list[Event])
 def get_events():
-    return sample_events
+    events = get_all_events()
+    return [to_event_schema(event) for event in events]
